8109316e5dd2ae069e117851d30110e83a57ccae
EgorKulakovskiy <142673330+EgorKylarovskiy@users.noreply.github.com>
2024-08-22 21:14:38 +0300

    mainwindow.cpp

---
+++ mainwindow.cpp
@@ -0,0 +1,66 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    ms = 0;
+    s = 0;
+    m = 0;
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT(TimerSlot()));
+    timer->start(1);
+    ui->pushButton->setText("start");
+    ui->pushButton_2->setText("AC");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::TimerSlot()
+{
+    ms++;
+    if(ms >= 1000) {
+        ms = 0;
+        s++;
+    }
+    if(s >= 60) {
+        s = 0;
+        m++;
+    }
+    ui->label->setText(QString::number(m) + " : ");
+    ui->label_2->setText(QString::number(s));
+    ui->label_3->setText(" : " + QString::number(ms));
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    flag = !flag;
+    if (flag){
+        ui->pushButton->setText("start");
+        timer->stop();
+    }
+    else
+    {
+        ui->pushButton->setText("Stop");
+        timer->start(1);
+    }
+
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ms = 0;
+    s = 0;
+    m = 0;
+
+    ui->label->setText(QString::number(m) + " : ");
+    ui->label_2->setText(QString::number(s));
+    ui->label_3->setText(" : " + QString::number(ms));
+}
+
